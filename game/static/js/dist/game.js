15a0e22335f8b5397425856115c6f84c81f99f3b
MOSSSSSSSSSSSSS <jj0723938@gmail.com>
2024-06-10 16:32:12 +0800

    canvas height player

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -110,6 +110,19 @@ class GameMap extends AcGameObject{
 		this.render();
 	}
 }
+class Player extends AcGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+        this.x = x;
+        this.y = y;
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.radius = radius;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.color = color;
+    }
+}
 class AcGamePlayground{
 	constructor(root){
 		this.root = root;
---
+++ game/static/js/src/playground/player/zbase.js
@@ -0,0 +1,13 @@
+class Player extends AcGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+        this.x = x;
+        this.y = y;
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.radius = radius;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.color = color;
+    }
+}
