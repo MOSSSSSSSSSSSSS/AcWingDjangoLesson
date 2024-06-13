5e1e2713af42645873f4f60c704b55b86ed580d2
MOSSSSSSSSSSSSS <jj0723938@gmail.com>
2024-06-13 17:22:31 +0800

    database Player

--- ljp1/settings.py
+++ ljp1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bz*e94xlwuae(0@h=1@qnda6564uqori)cm0gf03d59k6io9d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["49.235.143.83", "app6931.acapp.acwing.com.cn"]
 
