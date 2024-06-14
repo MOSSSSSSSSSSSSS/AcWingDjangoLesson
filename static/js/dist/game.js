4d43c8d7f2bcd06d64ee1218d65a0e925bcfeb4a
MOSSSSSSSSSSSSS <jj0723938@gmail.com>
2024-06-14 21:34:48 +0800

    login

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -420,6 +420,51 @@ class Settings{
         }
         this.username = "";
         this.photo = "";
+        this.$settings = $(`
+            <div class="ac-game-settings">
+                <div class="ac-game-settings-login">
+                    <div class="ac-game-settings-title">
+                        Login
+                    </div>
+                    <div class="ac-game-settings-username">
+                        <div class="ac-game-settings-item">
+                            <input type="text", placeholder="username">
+                        </div>
+                    </div>
+                    <div class="ac-game-settings-password">
+                        <div class="ac-game-settings-item">
+                            <input type="password", placeholder="password">
+                        </div>
+                    </div>
+                    <div class="ac-game-settings-submit">
+                         <div class="ac-game-settings-item">
+                            <button>Login</button>
+                        </div>
+                    </div>
+                    <div class="ac-game-settings-error-messages">
+                        error
+                    </div>
+                    <div class="ac-game-settings-option">
+                        register
+                    </div>
+                    <br>
+                    <div class="ac-game-settings-acwing">
+                        <img width="30" src="https://app6931.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                        <br>
+                        <div>
+                            AcWing Login
+                        </div>
+                    </div>
+                </div>
+                <div class="ac-game-settings-register">
+                </div>
+            </div>
+        `);
+        this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login.hide();
+        this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register.hide();
+        this.root.$ac_game.append(this.$settings);
 
         this.start();
     }
@@ -427,10 +472,12 @@ class Settings{
         this.getinfo();
     }
     login(){
-
+        this.$register.hide();
+        this.$login.show();
     }
     register(){
-
+        this.$login.hide();
+        this.$register.show();
     }
     getinfo(){
         let outer = this;
@@ -453,7 +500,10 @@ class Settings{
         })
     }
     hide(){
-
+        this.$settings.hide();
+    }
+    show(){
+        this.$settings.show();
     }
 }export class AcGame {
     	constructor(id, AcWingOS) {
